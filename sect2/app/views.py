263233bb2bb0fd203f8fa813f687e38087438d23
planetnxa <sc22amll@leeds.ac.uk>
2023-10-21 02:35:29 +0100

    week3

--- sect2/app/views.py
+++ sect2/app/views.py
@@ -1,9 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return "wag1!"
+    user = {"name":"JAaDE"}
+    return render_template("index.html", user=user)
 #so is this a view?? 
 # displayed in the web browser when the root document is requested from the server
 # what is a route and what is a decorator??
 # watch an intro vid to flask
+
+#okay so this is the thing that we see when we say run....but i don"t know how the structure works
+
+#is there a reason
+
+@app.route('/') #known as how to access it via the web page
+def displayFruit():
+    fruits = ["Apple","banana", "pineapple"]
+    return render_template("fruit.html",fruitss=fruits)
+    # because you have a function in the html sect ccalled fruitss, you need to define it here 
+    # in this python  document, fruits can ya dig it?
--- sect2/run.py
+++ sect2/run.py
@@ -1,3 +1,8 @@
-from app import appif __name__=="__main__":
-app.run(debug=True)
+from app import app
+if __name__=="__main__":
+    app.run(debug=True)
 # still confused, app like the folder or the actual app
+# so i need to be in sect
+# but also, i had to use python run.py because flask did not work
+# and what does adding to bash mean
+# and why does flask run not work......
