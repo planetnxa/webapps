0b3b2e0f0e4ac333f992370233a900f095b96ae8
marie ♡ <59845156+planetnxa@users.noreply.github.com>
2023-12-06 21:02:02 +0000

    poster upload

--- cw2/app/views.py
+++ cw2/app/views.py
@@ -4,7 +4,11 @@ from sqlalchemy import func
 #import db for context purposes, and place databse work in the actual application functions
 
 # initialise these at -1 so when we start we can differentiate between no input, and zero values
+"""
+how to ensure emails and ting
+layout
 
+"""
 
 @app.route("/") #the url for us to work with
 def index(): 
@@ -14,9 +18,25 @@ def index():
 def portfolio(): 
     return render_template("portfolio.html")
 
+@app.route("/port-abstudio") 
+def port_abstudio(): 
+    return render_template("port-abstudio.html")
+
+@app.route("/port-corazon") 
+def port_corazon(): 
+    return render_template("port-corazon.html")
+
+@app.route("/port-miakrylics") 
+def port_miaakrylics(): 
+    return render_template("port-miakrylics.html")
+
 @app.route("/book-me") 
 def book(): 
-        return render_template("book.html")
+    return render_template("book.html")
+
+@app.route("/book-conf") 
+def book_conf(): 
+    return render_template("book-confirmation.html")
 
 @app.route("/shop") 
 def shop(): 
@@ -25,5 +45,9 @@ def shop():
 @app.route("/admin") 
 def admin(): 
     return render_template("admin.html")
+
+@app.route("/admin-view") 
+def admin_orders(): 
+    return render_template("admin-view.html")
 
     # intialise the other balances and values etc
